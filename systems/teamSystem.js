049c4499da2b03d637460e365634c8b4ca516c74
ashdev <blackstar19488@gmail.com>
2026-02-26 06:23:51 +0100

    random captain if same elo

--- systems/teamSystem.js
+++ systems/teamSystem.js
@@ -2,6 +2,7 @@ const { getStats } = require("./eloSystem");
 
 function createBalancedTeams(playerIds) {
 
+    // Sort by elo descending
     const sorted = [...playerIds].sort(
         (a, b) => (getStats(b)?.elo || 1000) - (getStats(a)?.elo || 1000)
     );
@@ -9,7 +10,9 @@ function createBalancedTeams(playerIds) {
     const team1 = [];
     const team2 = [];
 
+    // Basic balance by average elo
     sorted.forEach(id => {
+
         const avg1 = team1.reduce((a,p)=>a+(getStats(p.id)?.elo||1000),0)/(team1.length||1);
         const avg2 = team2.reduce((a,p)=>a+(getStats(p.id)?.elo||1000),0)/(team2.length||1);
 
@@ -17,14 +20,27 @@ function createBalancedTeams(playerIds) {
         else team2.push({ id });
     });
 
-    // Captain = highest elo in team
-    const captain1 = team1.sort((a,b)=> (getStats(b.id)?.elo||1000)-(getStats(a.id)?.elo||1000))[0];
-    const captain2 = team2.sort((a,b)=> (getStats(b.id)?.elo||1000)-(getStats(a.id)?.elo||1000))[0];
-
-    captain1.isCaptain = true;
-    captain2.isCaptain = true;
+    assignCaptain(team1);
+    assignCaptain(team2);
 
     return { team1, team2 };
+}
+
+function assignCaptain(team) {
+
+    const maxElo = Math.max(
+        ...team.map(p => getStats(p.id)?.elo || 1000)
+    );
+
+    const highestPlayers = team.filter(
+        p => (getStats(p.id)?.elo || 1000) === maxElo
+    );
+
+    // 🎲 Random among highest ELO players
+    const randomIndex = Math.floor(Math.random() * highestPlayers.length);
+    const captain = highestPlayers[randomIndex];
+
+    captain.isCaptain = true;
 }
 
 module.exports = { createBalancedTeams };
