ca4c06509ec5c7239979d80eb3d9eb089ab33106
ashdev <blackstar19488@gmail.com>
2026-02-26 07:01:47 +0100

    FINAL FULL SYSTEM

---
+++ systems/champions.js
@@ -0,0 +1,7 @@
+const champions = [
+    "Bakko","Croak","Freya","Jamila","Raigon","Rook","Ruh'kaan","Shifu Thorn",
+    "Alysia","Ashka","Destiny","Ezmo","Iva","Jade","Jumong","Shen Rao","Taya","Varesh",
+    "Blossom","Lucie","Oldur","Peal","Pestilus","Poloma","Sirius","Ulric","Zander"
+];
+
+module.exports = { champions };
